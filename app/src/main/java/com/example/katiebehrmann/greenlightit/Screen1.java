2654af7a2414c8931ed77c08741d840263285620
katiebehrmann <kbehrmann@tc.columbia.edu>
2017-12-04 19:28:34 -0500

    First Push of GreenlightIt

---
+++ app/src/main/java/com/example/katiebehrmann/greenlightit/Screen1.java
@@ -0,0 +1,13 @@
+package com.example.katiebehrmann.greenlightit;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Screen1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen1);
+    }
+}
